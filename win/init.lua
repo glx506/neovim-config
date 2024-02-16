2e73b06a5c133fe76013c5f02f63b7859dd8f323
Grigory M. Gilevich <glx506@gmail.com>
2024-02-16 16:52:31 +0300

    Update 2024/02/16

--- win/init.lua
+++ win/init.lua
@@ -9,12 +9,11 @@
 -- 888   Y8888 Y8b.     Y88..88P   Y888P      888   888   "   888 --
 -- 888    Y888  "Y8888   "Y88P"     Y8P     8888888 888       888 --
 --                                                                --
--- Last update:06.04.2022                                         --
--- init.lua configuration file for NeoVIM                         --
+-- Last update:16.02.2024                                         --
+-- init.lua configuration file for NeoVIM SKB Edition             --
 -- Configured for: neovim-qt/neovim (Win 10 x64)                  --
 --------------------------------------------------------------------
 local map = vim.api.nvim_set_keymap
-
 vim.cmd [[set shada="NONE"]] -- Disable shada file.
 vim.cmd [[set viminfo="NONE"]] -- Disable viminfo file.
 vim.cmd [[set noswapfile]] -- Disable swap.
@@ -75,6 +74,8 @@ vim.o.listchars = "tab:<->,space:·,nbsp:␣,trail:•,eol:¶,precedes:«,extend
 -- In *.py files include smart indents after keywords:
 vim.cmd [[au BufRead *.py set smartindent cinwords=if,elif,else,for,while,try,except,finally,def,class]]
 ------------------------------------------------------------------------------------------------------------------------
+-- Tail:
+vim.cmd [[nnoremap <F5> :TailStart<cr>]]
 -- Switch Encoding:
 vim.cmd [[set wildmenu]]
 vim.cmd [[set wcm=<Tab>]]
@@ -93,12 +94,6 @@ vim.cmd [[menu Spell.Spell[EN] :set spell spelllang=en<cr>]]
 vim.cmd [[menu Spell.NoSpell :set spell nospell<cr>]]
 map('n', '<F11>', ':emenu Spell.<TAB>', {noremap = true, silent = true})
 ------------------------------------------------------------------------------------------------------------------------
--- NvimTree:
-require('nvim-tree').setup()
-map('n', '<F10>', ':NvimTreeToggle <cr>', {noremap = true, silent = true})
-map('v', '<F10>', '<esc>:NvimTreeToggle <cr>', {noremap = true, silent = true})
-map('i', '<F10>', '<esc>:NvimTreeToggle <cr>', {noremap = true, silent = true})
-------------------------------------------------------------------------------------------------------------------------
 -- Help Menu:
 vim.cmd [[set wildmenu]]
 vim.cmd [[set wildmode=longest:full]]
@@ -107,124 +102,10 @@ vim.cmd [[menu Help.Spelling&[F11] <esc>]]
 vim.cmd [[menu Help.Encoding&[F12] <esc>]]
 map('n', '<F1>', ':emenu Help.<TAB>', {noremap = true, silent = true})
 ------------------------------------------------------------------------------------------------------------------------
--- Lualine.nvim:
--- Download "Anonymice Nerd Font" (https://www.nerdfonts.com/font-downloads)
-require'lualine'.setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '>', right = '<'},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-    always_divide_middle = true,
-  }}
-------------------------------------------------------------------------------------------------------------------------
--- Comment.nvim
--- `gc` - Toggles the region using linewise comment
--- `gb` - Toggles the region using blockwise comment
-require('Comment').setup()
-------------------------------------------------------------------------------------------------------------------------
 -- Asociation of files with highlighting:
 -- Asociation of *.log files for syntax highlighting.
 vim.cmd [[au BufRead,BufNewFile *.log set filetype=log]]
 ------------------------------------------------------------------------------------------------------------------------
--- Python-Syntax:
-vim.cmd [[let g:python_highlight_all = 1]]
+vim.treesitter.language.register('python', 'someft')  -- the someft filetype will use the python parser and queries.
 ------------------------------------------------------------------------------------------------------------------------
--- Configuration autocomplete:
--- Set completeopt to have a better completion experience
--- Setup nvim-cmp.
-local cmp = require'cmp'
-
-cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
-  mapping = {
-    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
-
-------------------------------------------------------------------------------------------------------------------------
--- Configuration LSP:
-local nvim_lsp = require('lspconfig')
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-end
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'pyright' }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    }
-  }
-end
-------------------------------------------------------------------------------------------------------------------------
+require('lualine').setup()  -- LauLine
